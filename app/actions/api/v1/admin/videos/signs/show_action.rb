ebab2337b9cef3332bcdef628532b0ad3c4790c2
Vadim Shalamov <vadshalamov@gmail.com>
2018-08-13 16:47:26 +0400

    return signed url instead of signed fields

--- app/actions/api/v1/admin/videos/signs/show_action.rb
+++ app/actions/api/v1/admin/videos/signs/show_action.rb
@@ -22,15 +22,11 @@ module Api::V1::Admin::Videos::Signs
     end
 
     def sign(key)
-      Aws::S3::PresignedPost.new(
-        credentials,
-        ENV.fetch('AWS_REGION'),
-        ENV.fetch('AWS_S3_BUCKET_NAME'),
-        key: key,
-        acl: 'public-read',
-        content_type: 'application/zip',
-        success_action_status: '201'
-      ).fields
+      Aws::S3::Resource
+        .new(region: ENV.fetch('AWS_REGION'))
+        .bucket(ENV.fetch('AWS_S3_BUCKET_NAME'))
+        .object(key)
+        .presigned_url(:put, acl: 'public-read')
     end
   end
 end
--- app/controllers/api/v1/admin/videos/signs_controller.rb
+++ app/controllers/api/v1/admin/videos/signs_controller.rb
@@ -4,8 +4,8 @@ module Api::V1::Admin::Videos
   class SignsController < ::Api::V1::ApplicationController
     def show
       api_action do |m|
-        m.success do |signature|
-          render json: signature
+        m.success do |signed_url|
+          render json: {'signedUrl' => signed_url}
         end
       end
     end

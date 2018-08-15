02a59e22be18b706175f0bad597994bd84a330c7
Artem Gurzhii <artem.gurzhii@gmail.com>
2018-08-15 16:13:02 +0300

    add `video/avi` available video content type

--- lib/six_by_three/constants.rb
+++ lib/six_by_three/constants.rb
@@ -6,7 +6,7 @@ module SixByThree
     AVAILABLE_UPLOAD_IMAGE_CONTENT_TYPES = %w[image/png].freeze
 
     AVAILABLE_UPLOAD_VIDEO_EXTENSIONS = %w[mp4 avi mov].freeze
-    AVAILABLE_UPLOAD_VIDEO_CONTENT_TYPES = %w[video/mp4 video/x-msvideo video/quicktime].freeze
+    AVAILABLE_UPLOAD_VIDEO_CONTENT_TYPES = %w[video/mp4 video/x-msvideo video/quicktime video/avi].freeze
 
     VIDEO_FILE_SIZE_RANGE = 1..10.gigabytes
 

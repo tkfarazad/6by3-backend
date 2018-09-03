68de441115011a515d20ea6b6c47a804d9fcfdde
Artem Gurzhii <artem.gurzhii@gmail.com>
2018-09-03 19:36:45 +0200

    add `mpeg` to list of available video upload extensions

--- lib/six_by_three/constants.rb
+++ lib/six_by_three/constants.rb
@@ -5,8 +5,8 @@ module SixByThree
     AVAILABLE_UPLOAD_IMAGE_EXTENSIONS = %w[png].freeze
     AVAILABLE_UPLOAD_IMAGE_CONTENT_TYPES = %w[image/png].freeze
 
-    AVAILABLE_UPLOAD_VIDEO_EXTENSIONS = %w[mp4 avi mov].freeze
-    AVAILABLE_UPLOAD_VIDEO_CONTENT_TYPES = %w[video/mp4 video/x-msvideo video/quicktime video/avi].freeze
+    AVAILABLE_UPLOAD_VIDEO_EXTENSIONS = %w[mp4 avi mov mpeg].freeze
+    AVAILABLE_UPLOAD_VIDEO_CONTENT_TYPES = %w[video/mp4 video/x-msvideo video/quicktime video/avi video/mpeg].freeze
 
     VIDEO_FILE_SIZE_RANGE = 1..10.gigabytes
 

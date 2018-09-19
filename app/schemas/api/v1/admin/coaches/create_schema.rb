4fd104d512697e8309a51b021fc4a57cb6b522ee
Artem Gurzhii <artem.gurzhii@gmail.com>
2018-09-19 15:48:46 +0200

    allow to create coach with all attrs from the admin panel

--- app/schemas/api/v1/admin/coaches/create_schema.rb
+++ app/schemas/api/v1/admin/coaches/create_schema.rb
@@ -3,5 +3,17 @@
 module Api::V1::Admin::Coaches
   CreateSchema = Dry::Validation.Params(BaseSchema) do
     required(:fullname).filled(:str?)
+    optional(:avatar).filled(:image?)
+    optional(:featured).filled(:bool?)
+    optional(:personal_info).filled(:str?)
+    optional(:certifications).each(:str?)
+
+    optional(:social_links).schema do
+      optional(:facebook).filled(:str?)
+      optional(:twitter).filled(:str?)
+      optional(:instagram).filled(:str?)
+      optional(:linkedin).filled(:str?)
+      optional(:website).filled(:str?)
+    end
   end
 end

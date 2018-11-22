752f00a8d3ad739ed2bb00708bd8f656ac42b35b
Artem Gurzhii <artem.gurzhii@gmail.com>
2018-11-22 19:53:55 +0100

    add jpeg to the whitelist of image upload extenstions

--- lib/six_by_three/constants.rb
+++ lib/six_by_three/constants.rb
@@ -2,8 +2,8 @@
 
 module SixByThree
   module Constants
-    AVAILABLE_UPLOAD_IMAGE_EXTENSIONS = %w[png].freeze
-    AVAILABLE_UPLOAD_IMAGE_CONTENT_TYPES = %w[image/png].freeze
+    AVAILABLE_UPLOAD_IMAGE_EXTENSIONS = %w[png jpeg].freeze
+    AVAILABLE_UPLOAD_IMAGE_CONTENT_TYPES = %w[image/png image/jpeg].freeze
 
     AVAILABLE_UPLOAD_VIDEO_EXTENSIONS = %w[mp4 avi mov mpeg].freeze
     AVAILABLE_UPLOAD_VIDEO_CONTENT_TYPES = %w[video/mp4 video/x-msvideo video/quicktime video/avi video/mpeg].freeze

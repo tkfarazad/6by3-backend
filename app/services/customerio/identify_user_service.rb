e3253055067d1a4416eeb3126b11c8cb37256324
Vadim Shalamov <vadshalamov@gmail.com>
2018-12-20 16:17:55 +0500

    add cancelled attribute to customer.io
    https://app.asana.com/0/667886126022515/952992003457242

--- app/services/customerio/identify_user_service.rb
+++ app/services/customerio/identify_user_service.rb
@@ -4,7 +4,7 @@ module Customerio
   class IdentifyUserService
     include Import['customerio.client']
 
-    def call(user:)
+    def call(user:) # rubocop:disable Metrics/MethodLength
       client.identify(
         id: user.id,
         email: user.email,
@@ -13,7 +13,8 @@ module Customerio
         created_at: user.created_at.to_i,
         email_confirmed: user.email_confirmed_at.present?,
         card_added: card_added?(user),
-        plan_type: user.plan_type
+        plan_type: user.plan_type,
+        cancelled: cancelled?(user)
       )
     end
 
@@ -21,6 +22,18 @@ module Customerio
 
     def card_added?(user)
       !user.payment_sources_dataset.empty?
+    end
+
+    def cancelled?(user)
+      subscription =
+        user
+        .subscriptions_dataset
+        .order(:created_at)
+        .last
+
+      return false if subscription.nil?
+
+      subscription.status == SC::Billing::Stripe::Subscription::CANCELED_STATUS
     end
   end
 end
--- spec/services/customerio/indentify_user_service_spec.rb
+++ spec/services/customerio/indentify_user_service_spec.rb
@@ -10,9 +10,11 @@ RSpec.describe Customerio::IdentifyUserService, :customerio do
 
     let(:user) { create(:user) }
 
-    it 'identifies user in customerio' do
+    before do
       allow(customerio_client).to receive(:identify)
+    end
 
+    it 'identifies user in customerio' do
       call
 
       expect(customerio_client).to(
@@ -25,10 +27,104 @@ RSpec.describe Customerio::IdentifyUserService, :customerio do
             :created_at,
             :email_confirmed,
             :card_added,
-            :plan_type
+            :plan_type,
+            :cancelled
           )
         )
       )
+    end
+
+    context 'card_added attribute' do
+      context 'when card is not added' do
+        it 'identifies with proper value' do
+          call
+
+          expect(customerio_client).to(
+            have_received(:identify).with(
+              hash_including(card_added: false)
+            )
+          )
+        end
+      end
+
+      context 'when card is added' do
+        before do
+          create(:stripe_payment_source, user: user)
+        end
+
+        it 'identifies with proper value' do
+          call
+
+          expect(customerio_client).to(
+            have_received(:identify).with(
+              hash_including(card_added: true)
+            )
+          )
+        end
+      end
+    end
+
+    context 'cancelled attribute' do
+      context 'when subscription does not exist' do
+        it 'identifies with proper value' do
+          call
+
+          expect(customerio_client).to(
+            have_received(:identify).with(
+              hash_including(cancelled: false)
+            )
+          )
+        end
+      end
+
+      context 'when subscription is active' do
+        before do
+          create(:stripe_subscription, :active, user: user)
+        end
+
+        it 'identifies with proper value' do
+          call
+
+          expect(customerio_client).to(
+            have_received(:identify).with(
+              hash_including(cancelled: false)
+            )
+          )
+        end
+      end
+
+      context 'when subscription is canceled' do
+        before do
+          create(:stripe_subscription, :canceled, user: user)
+        end
+
+        it 'identifies with proper value' do
+          call
+
+          expect(customerio_client).to(
+            have_received(:identify).with(
+              hash_including(cancelled: true)
+            )
+          )
+        end
+      end
+
+      context 'when user has several subscriptions' do
+        before do
+          create(:stripe_subscription, :active, user: user, created_at: Time.current - 2.days)
+          create(:stripe_subscription, :canceled, user: user, created_at: Time.current - 1.day)
+        end
+
+        it 'identifies with proper value' do
+          call
+
+          expect(customerio_client).to(
+            have_received(:identify).with(
+              hash_including(cancelled: true)
+            )
+          )
+        end
+      end
     end
   end
 end
